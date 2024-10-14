396dc5db7e9f1320458b58e4091279e5eb30f4a1
soyoung1832 <68853692+soyoung1832@users.noreply.github.com>
2024-10-14 17:40:05 +0900

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -148,8 +148,8 @@
 </template>
 
 <script>
-import fullLogo from 'src/assets/images/navbar-full-rich.png'; // 전체 로고
-import collapsedLogo from 'src/assets/images/navbar-rich.png'; // 축소 로고
+import fullLogo from '@/assets/images/navbar-full-rich.png'; // 전체 로고
+import collapsedLogo from '@/assets/images/navbar-rich.png'; // 축소 로고
 export default {
   data() {
     return {
