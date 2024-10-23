d074bf964166f3d5553bccacc8c4983d158988f2
J <jisu0259@naver.com>
2024-10-23 13:16:04 +0900

    Refactor budget list

---
+++ src/components/budget/budgetlist.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Budgets</h2>
+      <ul>
+        <BudgetItem 
+          v-for="budget in budgets" 
+          :key="budget.id" 
+          :budget="budget"
+          @delete="onDelete"
+          @edit="onEdit"
+        />
+      </ul>
+    </div>
+</template>
+  
+<script>
+  import BudgetItem from './BudgetItem.vue'
+  
+  export default {
+    props: ['budgets'],
+    components: {
+      BudgetItem
+    },
+    methods: {
+      onDelete(budgetId) {
+        this.$emit('delete', budgetId);
+      },
+      onEdit(budget) {
+        this.$emit('edit', budget);
+      }
+    }
+  }
+  </script>
+  
